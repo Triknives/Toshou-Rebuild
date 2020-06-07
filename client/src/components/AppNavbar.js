6fc255c557956a07f795db61d2a44f7a57c27a9b
Triknives <cordell.desu@gmail.com>
2020-06-06 18:41:57 -0700

    update styling, adding routing component for authorized routes and guest routes

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,80 +1,67 @@
 import React, {Component, Fragment} from 'react';
 import{
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  Container,
+  Navbar
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import RegisterModal from './auth/RegisterModal';
-import Logout from './auth/Logout';
-import Login from './auth/Login';
+import {logout} from '../actions/authActions';
 
+const appNavbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <ul>
+      <li>
+        <a href='/profiles'><i class="fas fa-users" />{' '}Community Members</a>
+      </li>
+      <li>
+        <a href='/posts'>Message Board</a>
+      </li>
+      <li>
+        <a href='/dashboard'>
+        <i class="fas fa-user-tie" />{' '}
+          <span className='hide-sm'>Profile</span>
+        </a>
+      </li>
+      <li>
+        <a onClick={logout} href='#!'>
+          <i className='fas fa-sign-out-alt' />{' '}
+          <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
+    </ul>
+  );
 
-class AppNavbar extends Component {
-  state = {
-    isOpen: false,
-  };
+  const guestLinks = (
+    <ul>
+      <li>
+        <a href='/register'>Register</a>
+      </li>
+      <li>
+        <a href='/login'>Login</a>
+      </li>
+    </ul>
+  );
 
+  return (
+    <nav className='navbar bg-dark'>
+      <h1>
+        <a href='/'>
+          iiSho
+        </a>
+      </h1>
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
+    </nav>
+  );
+};
 
-  static propTypes = {
-    auth: PropTypes.object.isRequired,
-  }
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
 
-  toggle = () => {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  render() {
-    const { isAuthenticated, user } = this.props.auth;
-
-    const authLinks = (
-      <Fragment>
-      <NavItem>
-        <span className='navbar-text mr-3'>
-        <strong>{ user ? `Welcome ${user.name}` : ''}</strong>
-        </span>
-      </NavItem>
-        <NavItem>
-          <Logout/>
-        </NavItem>
-      </Fragment>
-    );
-
-    const guestLinks = (
-      <Fragment>
-      <NavItem>
-      </NavItem>
-      <NavItem>
-      </NavItem>
-      </Fragment>
-    );
-
-    return(
-      <div>
-      <Navbar color="black" dark expand="sm" className="mb-5" id="nav">
-      <Container>
-      <NavbarBrand href="/">Toshou</NavbarBrand>
-      <NavbarToggler onClick={this.toggle} />
-      <Collapse isOpen={this.state.isOpen} navbar>
-      <Nav className= "ml-auto" navbar>
-      { isAuthenticated ? authLinks : guestLinks}
-      </Nav>
-      </Collapse>
-      </Container>
-      </Navbar>
-      </div>
-    );
-  }
-}
 const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps, null)(AppNavbar);
+export default connect(mapStateToProps, { logout })(appNavbar);
---
+++ client/src/components/routing/Routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Register from '../auth/Register';
+import Login from '../auth/Login';
+import Alert from '../layout/Alert';
+import PrivateRoute from '../routing/PrivateRoute';
+
+const Routes = () => {
+  return (
+    <section className='container'>
+      <Alert />
+      <Switch>
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+        <PrivateRoute exact path='/posts/:id' component={Post} />
+        <Route component={NotFound} />
+      </Switch>
+    </section>
+  );
+};
+
+export default Routes; 
